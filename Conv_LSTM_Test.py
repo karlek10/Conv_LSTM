a2f90534bdcc5d537e05675b81b886d739287b45
Karlo Leskovar <karlo.leskovar10@gmail.com>
2021-03-26 14:28:28 +0100

    Update Conv_LSTM_Test.py

--- Conv_LSTM_Test.py
+++ Conv_LSTM_Test.py
@@ -9,12 +9,22 @@ import pandas as pd
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
 import torch
+import datetime
 
 
 
 
 class Meteo_MODIS_ds(Dataset):
-    def __init__(self, csv_path, raster_path, seq_len_in, seq_len_out, input_size):
+    def __init__(self, 
+                 csv_path, 
+                 raster_path, 
+                 seq_len_in, 
+                 seq_len_out, 
+                 input_size,
+                 train_end = "2000-11-01", 
+                 val_start="2000-11-01", 
+                 test_start="2001-01-01",
+                 **kwargs):
         """
         Parameters
         ----------
@@ -23,33 +33,48 @@ class Meteo_MODIS_ds(Dataset):
         is_valid (int): 1 for valid & 0 for training
         """        
         self.tabular_data = pd.read_csv(csv_path)
+        self.tabular_input = self.tabular_data[self.tabular_data.columns[2:]]
+        self.dates = self.tabular_data[self.tabular_data.columns[0]].astype("datetime64[ns]")
         self.raster_data = np.load(raster_path, allow_pickle=True)
         self.y = self.tabular_data[self.tabular_data.columns[1]] # observed runoff column
         self.seq_len_in = seq_len_in
         self.seq_len_out = seq_len_out
         self.input_size = input_size
+        
+        
+        print (self.dates.index)
 
     def __len__(self):
-        return self.tabular_data.__len__() - (self.seq_len_in - 1)
+        return self.dates.__len__() - (self.seq_len_in - 1)
 
     
     def __getitem__(self, index):
         ras_shape1, ras_shape2 = self.raster_data[:,1][0].shape # getting the raster shape 
-        tabular_data = np.array(self.tabular_data[self.tabular_data.columns[2:]]).astype("float32") # selecting tabular trainign data
+        tabular_input = np.array(self.tabular_input).astype("float32") # selecting tabular trainign data
         rasters = np.vstack(self.raster_data[:,1]).astype("float32").reshape(-1, ras_shape1, ras_shape2 ) # reshaping the raster 
         y =  np.array(self.y).astype("float32") # converting 
         # prepairing index + seq_len_in data
-        tab_data = tabular_data[index : index + seq_len_in]
+        tabular_input = tabular_input[index : index + seq_len_in]
         ras_data = rasters[index : index + seq_len_in]
         y_data =y[index + seq_len_in -1: index + seq_len_in + seq_len_out-1]
-
-        return {
+        
+        data = {
             "raster": torch.tensor(ras_data),
-            "tabular_data": torch.tensor(tab_data, dtype=torch.float32),
+            "tabular_data": torch.tensor(tabular_input, dtype=torch.float32),
             "runoff": torch.from_numpy(y_data),
             }
+        
+        return data
 
- 
+    # def train_val_dataset(dataset, val_split=0.25):
+    #     train_idx, val_idx = train_test_split(list(range(len(dataset))), test_size=val_split)
+    #     datasets = {}
+    #     datasets['train'] = Subset(dataset, train_idx)
+    #     datasets['val'] = Subset(dataset, val_idx)
+    #     return datasets
+    
+    
+
     
  # ====== STATICS ======
 num_batches = 32                # how many timeserieses to be trained in one iteration
@@ -67,25 +92,19 @@ train_data = Meteo_MODIS_ds(csv_path="input_data/Drava_data.csv",
                             raster_path="input_data/test_snow_data.npy",
                             seq_len_in = seq_len_in,
                             seq_len_out= seq_len_out,
-                            input_size = input_size,
-
-                          )
-
-
+                            input_size = input_size,)
 
 training_dataloader = DataLoader(train_data,
                         batch_size=5,
-                        drop_last=True
-                        )
+                        drop_last=True,)
+print ("The legth of the dataloader is:", len(training_dataloader))
 
 
 
 
 
 if __name__ == "__main__":
-    dataset = next(iter(training_dataloader))
-    print (len(training_dataloader))
-   
+    dataset = next(iter(training_dataloader))   
     print ("y_data shape:", dataset["runoff"].shape)
     print ("tabular input data shape:", dataset["tabular_data"].shape)
     print ("raster data input shape:", dataset["raster"].shape)
