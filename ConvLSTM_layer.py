f364495d698fe195d4d390525eb02534cb13c774
Karlo Leskovar <65714326+karlek10@users.noreply.github.com>
2021-08-23 13:11:59 +0200

    updates

---
+++ ConvLSTM_layer.py
@@ -0,0 +1,215 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  8 06:52:38 2021
+
+@author: karlo
+"""
+
+import torch
+import torch.nn as nn
+
+
+
+
+class ConvLSTMCell(nn.Module):
+    def __init__(self, input_size, hidden_size, kernel_size, bias = True,):
+        """
+        Initialize ConvLSTM Cell.
+
+        Parameters
+        ----------
+        input_dim : int
+            Number of color channels in input raster. (1 for MODIS)
+        hidden_size : int
+            Number of neurons in the hidden layer.
+        kernel_size : (int, int)
+            Convolutional kernel size.
+        bias : bool
+            Adding bias.
+
+        Returns
+        -------
+        new hidden_state, new cell_state
+        """
+        super(ConvLSTMCell, self).__init__()
+        
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        
+        self.kernel_size = kernel_size
+        self.padding = self.kernel_size[0] // 2, self.kernel_size[1] // 2
+        self.bias = bias
+                
+        self.conv2d = nn.Conv2d(in_channels = self.input_size + self.hidden_size, 
+                                out_channels = 4 * self.hidden_size,
+                                kernel_size = self.kernel_size,
+                                padding = self.padding,
+                                bias = self.bias)
+        
+    def forward(self, input_tensor, current_state, prints = True):
+        
+        h_state, c_state = current_state # hidden and cell states
+        if prints: print ("input_shape into each neuron: ", input_tensor.shape,
+                          "batch_size, channels, x-dim, y-dim")
+        combined = torch.cat((input_tensor, h_state), dim=1) # concatenate xt and h(t-1)
+        if prints: print ("combined input_shape: ", combined.shape)
+        conv_comb = self.conv2d(combined)
+        if prints: print ("conv_comb_shape: ", conv_comb.shape)
+
+        cc_i, cc_f, cc_o, cc_g = torch.split(conv_comb, self.hidden_size, dim=1)
+        # splitting the tensor to 4 gates
+        i = torch.sigmoid(cc_i)
+        f = torch.sigmoid(cc_f)
+        o = torch.sigmoid(cc_o)
+        g = torch.tanh(cc_g)
+        # calculating gate outputs
+        c_next = f * c_state + i * g # calculating new cell_state
+        h_next = o * torch.tanh(c_next) # calculating new hidden state
+        
+        return h_next, c_next
+
+    def init_hidden(self, batch_size, input_shape):        
+        return (torch.zeros(batch_size, self.hidden_size, input_shape[0], 
+                            input_shape[1], device=self.conv2d.weight.device),
+                torch.zeros(batch_size, self.hidden_size, input_shape[0], 
+                            input_shape[1], device=self.conv2d.weight.device))
+        
+
+class ConvLSTM_layer(nn.Module):
+    
+    """
+    Parameters:
+        input_dim: Number of color channels in input raster.
+        hidden_size: Number of neurons in the hidden layer.
+        kernel_size: Convolutional kernel size.
+        num_layers: Number of LSTM layers stacked on each other
+        batch_first: Whether or not dimension 0 is the batch or not
+        bias: Bias or no bias in Convolution
+        return_all_layers: Return the list of computations for all layers
+        Note: Will do same padding.
+    Input:
+        A tensor of size B, T, C, H, W or T, B, C, H, W
+    Output:
+        A tuple of two lists of length num_layers (or length 1 if return_all_layers is False).
+            0 - layer_output_list is the list of lists of length T of each output
+            1 - last_state_list is the list of last states
+                    each element of the list is a tuple (h, c) for hidden state and memory
+    Example:
+        >> x = torch.rand((32, 10, 64, 128, 128))
+        >> convlstm = ConvLSTM(64, 16, 3, 1, True, True, False)
+        >> _, last_states = convlstm(x)
+        >> h = last_states[0][0]  # 0 for layer index, 0 for h index
+    """
+
+    def __init__(self, input_size, hidden_size, kernel_size, num_layers, 
+                 batch_first = True, bias = True, ):
+        super(ConvLSTM_layer, self).__init__()
+        
+        self._check_kernel_size_consistency(kernel_size)
+        
+        # Checking if both kernel_size and hidden_size are lists having lentgh == num_layers
+        kernel_size = self._extend_for_multilayer(kernel_size, num_layers)
+        hidden_size = self._extend_for_multilayer(hidden_size, num_layers)
+        
+        if not len(kernel_size) == len(hidden_size) == num_layers:
+            raise ValueError("Inconsistent list length.")
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.kernel_size = kernel_size
+        self.num_layers = num_layers
+        self.batch_first = batch_first
+        self.bias = bias
+
+        cell_list = list()
+        for l in range(0, self.num_layers):
+            curr_input_size = self.input_size if l == 0 else self.hidden_size[l - 1]
+            
+            cell_list.append(ConvLSTMCell(input_size = curr_input_size,
+                                          hidden_size = self.hidden_size[l],
+                                          kernel_size = self.kernel_size[l],
+                                          bias = self.bias))
+            
+        self.cell_list = nn.ModuleList(cell_list)
+        
+    def forward(self, input_tensor, hidden_state=None, prints = True):
+        """
+        Parameters
+        ----------
+        input_tensor: 
+            5-D Tensor either of shape (t, b, c, h, w) or (b, t, c, h, w)
+        hidden_state: 
+            None. todo implement stateful
+        Returns
+        -------
+        last_state_list, layer_output
+        """ 
+        
+        
+        if not self.batch_first:
+            # (timesteps, batch_size, channels, height, weight) -> (b, t, c, h, w)
+            input_tensor = input_tensor.permute(1, 0, 2, 3, 4)
+        
+        b, seq_len, _, h, w = input_tensor.size()
+        #if prints: print("input_tensor shape:", input_tensor.shape, "[num_batches, seq_len, channels, height, width]")
+        if hidden_state is not None:
+            raise NotImplementedError()
+        else:
+            # Since the init is done in forward, send image_size here
+            hidden_state = self.get_init_hidden(batch_size = b, 
+                                                input_size = (h, w))
+        
+        layer_output_list = list()
+        last_state_list = list()
+        
+        cur_layer_input = input_tensor
+        
+        for layer_idx in range(self.num_layers):
+            h, c = hidden_state[layer_idx]
+            output_inner = list()
+            for t in range(seq_len):
+                h, c = self.cell_list[layer_idx](input_tensor=cur_layer_input[:, t, :, :, :],
+                                                 current_state = [h, c])
+                output_inner.append(h) # list
+            
+              
+            layer_output = torch.stack(output_inner, dim=1) # tensor
+            #if prints: print("SHAPE of LAYER-output is: ", layer_output.shape)
+            cur_layer_input = layer_output
+            layer_output_list.append(layer_output)
+            last_state_list.append([h, c])  
+            #if prints: print("LAYER_output list len: ", len(layer_output_list))
+            #
+        layer_output_list = layer_output_list[-1:]
+        last_state_list = last_state_list[-1:]
+        
+        if prints: print("LAYER_output list shape: ", layer_output_list[0].shape)
+
+
+     
+        # if prints: print("final ConvLSTM output shape:", last_state_tensor.shape, "[num_batches, seq_len, height, width]")
+        # if prints: print ("type of the ConvLSTM output is: ", type(last_state_tensor))
+        # if prints: print ("len of the ConvLSTM output: ", len(last_state_tensor))           
+            
+        return layer_output_list, last_state_list
+
+
+
+    
+    def get_init_hidden(self, batch_size, input_size,):
+        init_states = list()
+        for i in range(self.num_layers):
+            init_states.append(self.cell_list[i].init_hidden(batch_size, input_size,))
+        return init_states
+        
+
+    @staticmethod
+    def _check_kernel_size_consistency(kernel_size):
+        if not (isinstance(kernel_size, tuple) or 
+                (isinstance(kernel_size, list) and all([isinstance(elem, tuple) for elem in kernel_size]))):
+            raise ValueError(" 'kernel_size' must be a tuple or list of tuples.")
+    @staticmethod
+    def _extend_for_multilayer(param, num_layers):
+        if not isinstance(param, list):
+            param = [param] * num_layers
+        return param 
--- Conv_LSTM_Test.py
+++ Conv_LSTM_Test.py
@@ -3,14 +3,23 @@
 Created on Mon Mar 22 13:00:45 2021
 
 @author: karlo
-"""
 
+# The code was revised from repos: https://github.com/ndrplz/ConvLSTM_pytorch;
+https://github.com/KimUyen/ConvLSTM-Pytorch/blob/master/convlstm.py
+
+"""
+import sys
+sys.path.append('D:\\OneDrive\Python\\11_LSTM_RNN\\')
 import pandas as pd
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
 import torch
 import datetime
 from sklearn.preprocessing import MinMaxScaler
+import torch.nn as nn
+from torch.autograd import Variable
+from ConvLSTM_layer import ConvLSTM_layer
+from utility_functions import LSTM
 
 
 
@@ -25,7 +34,6 @@ class Meteo_MODIS_ds(Dataset):
                  date_end = None,
                  ):
         """
-        
         Parameters
         ----------
         csv_path (string): 
@@ -70,7 +78,7 @@ class Meteo_MODIS_ds(Dataset):
                 print ("Starting date is valid.\nStarting day set to {}."\
                         .format(self.date_start.strftime('%d.%m.%Y')))
             else:
-                raise Exception("The provided start date is not in range!")
+                print ("The provided start date is not in range!")
         else: # setting start date to first value in date
             self.date_start = self.dates.iloc[0]
             print ("Starting date was not provided.\nFirst day of dataset ({}) was set as starting date."\
@@ -99,18 +107,17 @@ class Meteo_MODIS_ds(Dataset):
         
         tabular_input = np.array(self.tabular_input).astype("float32")[self.start_idx:self.end_idx] 
         # selecting tabular trainign data
-        rasters = torch.tensor(np.vstack(self.raster_data[:,1]).astype("float32").reshape(-1, ras_shape1, ras_shape2 ))[self.start_idx:self.end_idx] 
+        rasters = torch.tensor(np.vstack(self.raster_data[:,1]).astype("float32").reshape(-1, ras_shape1, ras_shape2))[self.start_idx:self.end_idx] 
         # reshaping the raster            
         y =  np.array(self.y).astype("float32")[self.start_idx:self.end_idx][index + seq_len_in-1: index + seq_len_in + seq_len_out-1] 
         # reshaping observed runoff 
-        y_scaler = MinMaxScaler()
         tab_scaler = MinMaxScaler()
 
         tabular_scaled = tab_scaler.fit_transform(tabular_input)[index : index + seq_len_in] # rescaling [0, 1]
-        _ = y_scaler.fit_transform(y.reshape(-1,1)) # rescaling [0, 1]
+        y = y.reshape(-1,1) # rescaling [0, 1]
         rasters_scaled = (rasters/rasters.unique().max())[index : index + seq_len_in]
         """Rasters are divided by largest unique value (label) in order to get labels
-        between 0 and 1 for faster training anf better results."""
+        between 0 and 1 for faster training and better results."""
         data = {
             "raster": rasters_scaled,
             "tabular_data": torch.tensor(tabular_scaled, dtype=torch.float32),
@@ -118,15 +125,117 @@ class Meteo_MODIS_ds(Dataset):
             }
         return data
 
-    
 
-    
+
+
+"""
+
+class Conv_LSTM_runoff(nn.Module):
+    def __init__(self, input_size_lstm, hidden_size_lstm, num_layers_lstm, 
+                 seq_len_out_lstm, device, input_size_conv, hidden_size_conv, 
+                 kernel_size, num_layers_conv, seq_len_out,  
+                 return_all_layers_conv = False, 
+                 dropout_lstm=0.3, dropout_conv = 0.3):
+        super(Conv_LSTM_runoff, self).__init__()
+        self.input_size_lstm = input_size_lstm
+        self.hidden_size_lstm = hidden_size_lstm
+        self.num_layers_lstm = num_layers_lstm
+        self.seq_len_out_lstm = seq_len_out_lstm
+        self.device = device
+        self.input_size_conv = input_size_conv
+        self.hidden_size_conv = hidden_size_conv
+        self.kernel_size = kernel_size
+        self.num_layers_conv  = num_layers_conv
+        self.seq_len_out = seq_len_out
+        
+        # defining the network architecture
+        # Meteo data LSTM layer(s)
+        self.lstm = nn.LSTM(self.input_size_lstm, self.hidden_size_lstm, 
+                            self.num_layers_lstm, batch_first = True, 
+                            dropout=dropout_lstm)
+        
+        # ReLU activation - avoid linearity
+        self.relu_lstm = nn.ReLU()
+        
+        # MODIS snow data ConvLSTM layer(s)
+        self.conv_lstm = ConvLSTM(input_size=self.input_size_conv, 
+                                  hidden_size=self.hidden_size_conv, 
+                                  kernel_size=self.kernel_size, 
+                                  num_layers=self.num_layers_conv,
+                                  batch_first = True, bias = True, 
+                                  return_all_layers = False)
+        
+        # ReLU activation - avoid linearity
+        self.relu_conv = nn.ReLU()
+        
+        
+        # Fully connected Linear layer_1
+        # input = output_lstm + output_ConvLSTM
+        self.linear1 = nn.Linear(128, 64)
+        
+        # Fully connected Linear layer_2
+        self.linear2 = nn.Linear(64, 32)
+        
+        # Fully connected Linear layer_2
+        self.linear2 = nn.Linear(32, self.seq_len_out)
+        
+        
+        
+"""
+
+
+
+
+
+
+
+"""
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, seq_len_out, device, dropout=0.1):
+        super(LSTM, self).__init__()
+        # Initializing the model parameters
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.seq_len_out = seq_len_out
+        self.device = device
+        # Layer 1: LSTM # batch_size first ()
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first = True, dropout=dropout)
+        # # Layer 2: Fully coneccted (linear) layer
+        self.linear = nn.Linear(self.hidden_size, self.seq_len_out)
+    def forward(self, input_seq, prints = False):
+        # Reshaping the input_seq
+        input_seq = input_seq.view(-1, input_seq.shape[1], self.input_size)
+        if prints: print("input_seq shape:", input_seq.shape, "->[num_batches, seq_len, num_features]")     
+        # LSTM 
+        output, (h_state, c_state) = self.lstm(input_seq)
+        if prints: print("LSTM: output shape:" , output.shape, "->[num_batches, seq_len, hidden_size]", 
+                         "\n " "LSTM: h_state shape:", h_state.shape, 
+                          "->[num_layers*num_directions, num_batches, hidden_size]", "\n"
+                          "LSTM: c_state shape:", c_state.shape, 
+                          "->[num_layers*num_directions, num_batches, hidden_size]")
+        # Reshaping to take last tensot as output
+        output = output[:, -1, :]
+        if prints: print("Output reshaped:", output.shape, "->[num_batches, hidden_size]")
+        # Fully connected layer
+        output = self.linear(output)
+        if prints: print("FNN: Final outpu shape:", output.shape, "->[num_batches, num_features]")
+        return output
+
+
+"""
+
+
+
+
+
+ 
  # ====== VARIABLES ==============
-batch_size = 10                # how many timeserieses to be trained in one iteration
+batch_size = 12                # how many timeserieses to be trained in one iteration
 input_size = 9                  # number of features (stations) --> rain, temp, hum, etc. 
-hidden_size = 30                # number of hidden neurons
+hidden_size = 25                # number of hidden neurons
 num_layers = 2                  # number of LSTM layers
-seq_len_in = 5                # length of the training time series
+seq_len_in = 17               # length of the training time series
 seq_len_out = 1                 # number of output step for predicted runoff
 train_start = "2000-04-02"      # start date of training period
 train_end = "2000-08-31"        # end date of training period
@@ -134,6 +243,10 @@ val_start = "2000-09-01"        # start date of validation period
 val_end = "2000-11-30"          # end date of validationperiod
 test_start = "2000-12-01"       # testing period start date
 test_end = "2001-01-12"         # end date of testing period
+
+shape = (65,97)
+channels = 1
+kernel = (5,5)
 # ==================================
     
 train_data = Meteo_MODIS_ds(csv_path="input_data/Drava_data.csv",
@@ -146,11 +259,41 @@ train_data = Meteo_MODIS_ds(csv_path="input_data/Drava_data.csv",
                             date_end=train_end
                             )
 
+
+
 training_dataloader = DataLoader(train_data,
                         batch_size=batch_size,
                         drop_last=True,)
-print ("The legth of the dataloader is:", len(training_dataloader))
 
+data_sample = next(iter(training_dataloader)) 
+
+input_test_ConvLSTM = Variable(torch.rand(batch_size, seq_len_in, channels, shape[0], shape[1]))
+
+input_test_LSTM = Variable(torch.rand(batch_size, seq_len_in, input_size))
+# print ("      input_shape: ", input_test_ConvLSTM.shape)
+
+
+
+conv_lstm = ConvLSTM_layer(input_size=channels, hidden_size=hidden_size, kernel_size=kernel,
+                     num_layers=2, batch_first=True, bias=True, )
+
+
+lstm = LSTM(input_size, hidden_size, num_layers, seq_len_out, device="cuda")
+
+
+
+
+layer_output_list, last_state_list = conv_lstm(data_sample["raster"])
+
+output_lstm = lstm(input_test_LSTM, )
+
+
+
+
+#print ("The legth of the dataloader is:", len(training_dataloader))
+
+
+"""
 valid_data = Meteo_MODIS_ds(csv_path="input_data/Drava_data.csv",
                             raster_path="input_data/test_snow_data.npy",
                             
@@ -161,7 +304,7 @@ valid_data = Meteo_MODIS_ds(csv_path="input_data/Drava_data.csv",
                             date_end=val_end
                             )
 
-valid_dataloader = DataLoader(train_data,
+valid_dataloader = DataLoader(tup[:,1],
                         batch_size=batch_size,
                         drop_last=True,)
 
@@ -175,19 +318,23 @@ test_data = Meteo_MODIS_ds(csv_path="input_data/Drava_data.csv",
                             date_end=test_end
                             )
 
-test_dataloader = DataLoader(train_data,
+test_dataloader = DataLoader(tup[:,1],
                         batch_size=batch_size,
                         drop_last=True,)
 
 
-
-
+"""
 
 if __name__ == "__main__":
-    dataset = next(iter(training_dataloader))   
-    print ("y_data shape:", dataset["runoff"].shape)
-    print ("tabular input data shape:", dataset["tabular_data"].shape)
-    print ("raster data input shape:", dataset["raster"].shape)
+
+    
+    
+    data_sample = next(iter(training_dataloader))   
+    
+   #print (output_lstm)
+    #print ("y_data shape:", data_sample["runoff"].shape)
+    #print ("tabular input data shape:", data_sample["tabular_data"].shape)
+    #print ("raster data input shape:", data_sample["raster"][0][0].size())
     
        
     # print ("y_data shape:", dataset["runoff"])
